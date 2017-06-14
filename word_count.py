4dccb9d2459b76e86baeb5be89cb3631030e0127
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-14 11:09:16 +0300

    added some comments

--- word_count.py
+++ word_count.py
@@ -1,20 +1,22 @@
-# -*- coding: utf-8 -*
-import re
+
+"""
+Find the number  of  word occurences in a phrase or sentence
+takes one argument and returns a dictionary of words with their occurence
+"""
 
 def words(phrase):
-
-	list_of_words = phrase.strip()
 	occurence_dictionary = {}
-	  
-  	for word in list_of_words:
-  		if word.isdigit():
-  			occurence_dictionary[int(word)] = list_of_words.count(word)
-  		else:
-  			occurence_dictionary[word] = list_of_words.count(word)
+
+	list_of_words = phrase.split()
+
+	for word in list_of_words:
+
+		if word.isdigit():
+			occurence_dictionary[int(word)] = list_of_words.count(word)
+		else:
+			occurence_dictionary[word] = list_of_words.count(word)
+
 
 	return occurence_dictionary
 
-
-#print(words('NevEr\n , Never 1 , 1 , 3'))
-print(words('one fish two fish red fish blue fish'))
   
